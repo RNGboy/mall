1e3443639086a9cc8ae3ca3fbe3ecdcc0ec9bc74
如斯 <941653940@qq.com>
2022-01-05 17:18:11 +0800

    修改

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    target = 'http://152.136.185.210:8000'
+    target = 'http://152.136.185.210:8000/api/w6'
   }
 
   // 创建代理对象并转发请求
